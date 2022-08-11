17f9fec53e97023f74d850e0033fc971359cbb68
bartekkrzepkowski <bartekkrzepkowski@gmail.com>
2022-08-11 15:57:56 +0200

    report

--- trainers/distilTrainer_hf_collator.py
+++ trainers/distilTrainer_hf_collator.py
@@ -29,6 +29,18 @@ class DistilTrainer(object):
         self.device = device
 
     def run_exp(self, epoch_start, epoch_end, exp_name, config_run_epoch, temp=1.0, random_seed=42):
+        """
+        Main method of trainer.
+        Init df -> [Init Run -> [Run Epoch]_{IL} -> Update df]_{IL}]
+        {IL - In Loop}
+        Args:
+            epoch_start (int): A number representing the beginning of run
+            epoch_end (int): A number representing the end of run
+            exp_name (str): Base name of experiment
+            config_run_epoch (): ##
+            temp (float): CrossEntropy Temperature
+            random_seed (int): Seed generator
+        """
         save_path = self.at_exp_start(exp_name, random_seed)
         for epoch in tqdm(range(epoch_start, epoch_end), desc='run_exp'):
             self.teacher.eval()
@@ -40,6 +52,13 @@ class DistilTrainer(object):
         wandb.finish()
 
     def at_exp_start(self, exp_name, random_seed):
+        """
+        Initialization of experiment.
+        Creates fullname, dirs and loggers.
+        Args:
+            exp_name (str): Base name of experiment
+            random_seed (int): seed generator
+        """
         self.manual_seed(random_seed)
         print('is fp16?', self.accelerator.use_fp16)
         date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -51,10 +70,19 @@ class DistilTrainer(object):
         # wandb.tensorboard.patch(root_logdir=f'{base_path}/tensorboard', pytorch=True, save=True)
         wandb.watch(self.student, log_freq=1000, idx=0, log_graph=True, log='all', criterion=self.criterion1)
         self.t_logger = TensorboardPyTorch(f'{base_path}/tensorboard', self.device)
-        # self.n_logger = NeptuneLogger(exp_name)
         return save_path
 
     def run_epoch(self, epoch, save_path, config_run_epoch, phase, temp):
+        """
+        Init df -> [Init Run -> [Run Epoch]_{IL} -> Update df]_{IL}]
+        {IL - In Loop}
+        Args:
+            epoch (int): Current epoch
+            save_path (str): Path to save the model
+            config_run_epoch (): ##
+            phase (train|test): Phase of training
+            temp (float): CrossEntropy Temperature
+        """
         running_loss1 = 0.0
         running_loss2 = 0.0
         running_loss3 = 0.0
@@ -62,13 +90,11 @@ class DistilTrainer(object):
         running_denom = 0.0
         global_step = 0
         loader_size = len(self.loaders[phase])
-        # self.n_logger['epoch'].log(epoch)
         wandb.log({'epoch': epoch})
         progress_bar = tqdm(self.loaders[phase], desc=f'run_epoch: {phase}',
                             mininterval=30, leave=False, total=loader_size)
         for i, data in enumerate(progress_bar):
             global_step += 1
-            # self.n_logger['step'].log(i)
             wandb.log({'step': i}, step=global_step)
             input_ids, labels = data['input_ids'], data['labels']
 
@@ -96,18 +122,11 @@ class DistilTrainer(object):
             # jakies ważenie losów? może związane ze schedulerem?
             loss = (loss1 + loss2 + loss3) / 3
 
-            # self.n_logger['train_every_step1_mlm_loss'].log(loss1.item())
-            # self.n_logger['train_every_step2_distill_loss'].log(loss2.item())
-            # # self.n_logger['train_every_step3_cosine_loss'].log(loss3.item())
-            # self.n_logger['train_every_step'].log(loss.item())
-
-            # wandb
             wandb.log({'every_step/mlm': loss1.item(), 'every_step/distill': loss2.item(),
                        'every_step/cosine': loss3.item(), 'every_step/loss': loss.item()}, step=global_step)
 
             loss /= config_run_epoch.grad_accum_steps
             if 'train' in phase:
-                #loss.backward()
                 self.accelerator.backward(loss) # jedyne użycie acceleratora w trainerze, wraz z clip_grad_norm
                 if (i + 1) % config_run_epoch.grad_accum_steps == 0 or (i + 1) == loader_size:
                     self.accelerator.clip_grad_norm_(filter(lambda p: p.requires_grad, self.student.parameters()), 3.0)
@@ -137,11 +156,6 @@ class DistilTrainer(object):
                 if self.lr_scheduler is not None:
                     wandb.log({'lr_scheduler': self.lr_scheduler.get_last_lr()[0]}, step=global_step)
 
-                # self.n_logger[f'MLM Loss/{phase}'].log(losses[f'mlm/{phase}'], step=global_step)
-                # self.n_logger[f'Distil Loss/{phase}'].log(losses[f'distil/{phase}'], step=global_step)
-                # # self.n_logger[f'Cosine Loss/{phase}'].log(losses[f'cosine/{phase}'], step=global_step)
-                # self.n_logger[f'Loss/{phase}'].log(losses[f'loss/{phase}'], step=global_step)
-
                 self.t_logger.log_scalar(f'MLM Loss/{phase}', losses[f'mlm/{phase}'], global_step)
                 self.t_logger.log_scalar(f'Distil Loss/{phase}', losses[f'distil/{phase}'], global_step)
                 self.t_logger.log_scalar(f'Cosine Loss/{phase}', losses[f'cosine/{phase}'], global_step)
@@ -154,13 +168,23 @@ class DistilTrainer(object):
                 running_denom = 0.0
 
                 if (i + 1) % config_run_epoch.save_interval == 0 or (i + 1) == loader_size:
-                    self.save_student(save_path)
+                    self.save_student(save_path, i)
 
-    def save_student(self, path):
-        # torch.save(self.student.state_dict(), f"{path}/student_{datetime.datetime.utcnow()}.pth")
-        self.student.save_pretrained(f"{path}/student_{datetime.datetime.utcnow()}.pth")
+    def save_student(self, path, step):
+        """
+        Save the model.
+        Args:
+            save_path (str): Path to save the model
+            step (int): Step of the run
+        """
+        self.student.save_pretrained(f"{path}/model_{datetime.datetime.utcnow()}_step_{step}.pth")
 
     def manual_seed(self, random_seed):
+        """
+        Set the environment for reproducibility purposes.
+        Args:
+            random_seed (int): seed generator
+        """
         if 'cuda' in self.device.type:
             torch.cuda.empty_cache()
             torch.backends.cudnn.deterministic = True
